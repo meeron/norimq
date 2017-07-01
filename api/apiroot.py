7d553e891a9991bfe3976de1de8afbb730568a2e
Miron Jakubowski <mijakubowski@gmail.com>
2017-07-01 15:17:15 +0200

    Queues api structure

--- api/apiroot.py
+++ api/apiroot.py
@@ -8,9 +8,27 @@ class Api:
     def __init__(self):
         self.queues = Queues()
 
+    def config(self):
+        return {
+            '/queues': {
+                'request.dispatch': cherrypy.dispatch.MethodDispatcher()
+            }
+        }
+
+
+@cherrypy.expose
 class Queues:
     """Queues class"""
 
-    @cherrypy.expose
-    def index(self):
-        return "Queues"
+    def GET(self, name=None):
+        if name:
+            return "Queue {}".format(name)
+
+        return "Queues list"
+
+    def PUT(self, name):
+        cherrypy.response.headers["Status"] = "201"
+        return "PUT OK"
+
+    def DELETE(self, name):
+        return "DEL OK"
--- server.py
+++ server.py
@@ -7,8 +7,11 @@ from api import Api
 print(__file__)
 
 if __name__ == '__main__':
-    cherrypy.tree.mount(Home(), '/')
-    cherrypy.tree.mount(Api(), '/api')
+    home = Home()
+    api = Api()
+
+    cherrypy.tree.mount(home, '/')
+    cherrypy.tree.mount(api, '/api', api.config())
 
     cherrypy.engine.start()
     cherrypy.engine.block()
